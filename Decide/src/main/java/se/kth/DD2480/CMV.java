1325d0dd5e6a7b91ac2b2f4f2a040f5f722a5089
Teoman Köylüoglu <151879574+Laafayeette@users.noreply.github.com>
2026-01-22 11:49:50 +0100

    feat: add skeleton code for lics (#21)
    Creates initial boilerplate for defining lic methods, constructor and CMV array.

--- Decide/src/main/java/se/kth/DD2480/CMV.java
+++ Decide/src/main/java/se/kth/DD2480/CMV.java
@@ -1,4 +1,80 @@
 package se.kth.DD2480;
 
+/**
+ * Represents the CMV boolean array that is used to determine which LIC conditions are met.
+ */
 public class CMV {
+
+    private boolean[] cmv;
+
+
+    public CMV() {
+        this.cmv = new boolean[15];
+    }
+
+    private boolean lic0() {
+        return false;
+
+    }
+
+
+    private boolean lic1() {
+        return false;
+    }
+
+    private boolean lic2() {
+        return false;
+    }
+
+    private boolean lic3() {
+        return false;
+    }
+
+    private boolean lic4() {
+        return false;
+    }
+
+    private boolean lic5() {
+        return false;
+    }
+
+    private boolean lic6() {
+        return false;
+    }
+    private boolean lic7() {
+        return false;
+    }
+
+    private boolean lic8() {
+        return false;
+    }
+
+    private boolean lic9() {
+        return false;
+    }
+
+    private boolean lic10() {
+        return false;
+    }
+
+    private boolean lic11() {
+        return false;
+    }
+
+    private boolean lic12() {
+        return false;
+    }
+
+    private boolean lic13() {
+        return false;
+    }
+
+    private boolean lic14() {
+        return false;
+    }
+
+    private boolean lic15() {
+        return false;
+    }
+
 }
