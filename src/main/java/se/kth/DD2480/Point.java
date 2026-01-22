4d78a65c2081ceff6dbbfb0235d72afef75715c2
Ahmed <ahmed.baccar@epfl.ch>
2026-01-22 19:53:21 +0100

    feat: triangleArea method
    Add a method to calculate the Area of a triangle in the Point class.

--- src/main/java/se/kth/DD2480/Point.java
+++ src/main/java/se/kth/DD2480/Point.java
@@ -12,4 +12,12 @@ public class Point {
     double distance (Point p2){
         return Math.sqrt(Math.pow(this.x - p2.x, 2) + Math.pow(this.y - p2.y, 2));
     }
+
+    static double triangleArea(Point a, Point b, Point c) {
+        return 0.5 * Math.abs(
+                a.x * (b.y - c.y)
+                        + b.x * (c.y - a.y)
+                        + c.x * (a.y - b.y)
+        );
+    }
 }
