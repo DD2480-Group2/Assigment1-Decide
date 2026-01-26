7c39c0aa06ccc49a951841eb9902bd955ae61d4e
Samuel Peetre <peetre@kth.se>
2026-01-26 20:05:32 +0100

    refactor: Move MEC radius calculation into dedicated function (#9)
    Moves calculation of the minimal enclosing circle radius that contains three points from the LIC 1 function into its own dedicated function within the Point class. This allows for reuse in LIC 8 and 13 functions.

--- src/main/java/se/kth/DD2480/CMV.java
+++ src/main/java/se/kth/DD2480/CMV.java
@@ -29,25 +29,13 @@ public class CMV {
             return false;
         }
         for (int i = 0; i < NUMPOINTS-2; i++) {
-
-            //r = abc/4A where r is radius of the triangles Circumcircle, a,b,c is the length of the sides between the points and A is the triangles Area
             Point a = points[i];
             Point b = points[i+1];
             Point c = points[i+2];
-            double sideA = a.distance(b);
-            double sideB = b.distance(c);
-            double sideC = c.distance(a);
-            double area = Point.triangleArea(a, b, c);
-            if (area < 0.000001) {
-                if (sideA/2 > RADIUS1 || sideB/2 > RADIUS1 || sideC/2 > RADIUS1) {
-                    return true;
-                }
-            } else {
-                //r = abc/4A
-                double circumradius = (sideA*sideB*sideC)/(4*area);
-                if (circumradius > RADIUS1) {
-                    return true;
-                }
+
+            double mecRadius = (Point.minimalEnclosingCircleRadius(a, b, c));
+            if (mecRadius > RADIUS1) {
+                return true;
             }
         }
         return false;
