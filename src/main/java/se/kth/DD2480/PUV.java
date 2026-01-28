04b234637a4b3f4d7983a7c5075d48dde61796f3
Kevin Lov <Kevin.Looov@gmail.com>
2026-01-28 19:38:49 +0100

    feat: Adding setAllFalse (#87)
    Method that might be redundant since initial values for the arr is
    false. But this method can be used to prove that all values are false
    for testing the whole program.

--- src/main/java/se/kth/DD2480/PUV.java
+++ src/main/java/se/kth/DD2480/PUV.java
@@ -20,4 +20,13 @@ public class PUV {
     private void checkIndex(int i) {
         if (i < 0 || i > 14) throw new IndexOutOfBoundsException("Allowed index is [0, 14]");
     }
+
+    /**
+     * Used to make test where no LIC stops the launch. By using this method the FUV should be all true
+     */
+    public void setAllFalse() {
+        for (int i = 0; i < 15; i++) {
+            setIndex(i, false);
+        }
+    }
 }
