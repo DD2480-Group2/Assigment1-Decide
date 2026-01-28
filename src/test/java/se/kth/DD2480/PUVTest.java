9addf5560ad1dbe378becc633d6cd2d7b2a8e7b3
Kevin Lov <Kevin.Looov@gmail.com>
2026-01-28 19:54:51 +0100

    feat: tests for PUV class (#87)
    Tests all methods (getters and setters and also cosntructor) to make
    sure they work as intended.

--- src/test/java/se/kth/DD2480/PUVTest.java
+++ src/test/java/se/kth/DD2480/PUVTest.java
@@ -18,5 +18,78 @@ class PUVTest {
     void tearDown() {
     }
 
+    @Test
+    void constructor_initializesAllToFalse() {
+        for (int i = 0; i < 15; i++) {
+            assertFalse(puv.getIndex(i));
+        }
+    }
 
+    @Test
+    void setIndexAnd_getIndexWorkForValidIndices() {
+        PUV puv = new PUV();
+
+        puv.setIndex(0, true);
+        puv.setIndex(14, true);
+        puv.setIndex(7, false);
+
+        assertTrue(puv.getIndex(0));
+        assertTrue(puv.getIndex(14));
+        assertFalse(puv.getIndex(7));
+    }
+
+    @Test
+    void setAllFalseSetsAllValuesToFalse() {
+        PUV puv = new PUV();
+
+        puv.setIndex(0, true);
+        puv.setIndex(14, true);
+        puv.setIndex(7, true);
+
+        puv.setAllFalse();
+
+        for (int i = 0; i < 15; i++) {
+            assertFalse(puv.getIndex(i));
+        }
+    }
+
+    @Test
+    void setIndexThrowsWhenIndexTooLow() {
+        PUV puv = new PUV();
+
+        IndexOutOfBoundsException error = assertThrows(IndexOutOfBoundsException.class, () ->
+                puv.setIndex(-1, true));
+
+        assertEquals("Allowed index is [0, 14]", error.getMessage());
+    }
+
+    @Test
+    void setIndexThrowsWhenIndexTooHigh() {
+        PUV puv = new PUV();
+
+        IndexOutOfBoundsException error = assertThrows(IndexOutOfBoundsException.class, () ->
+                puv.setIndex(15, true));
+
+        assertEquals("Allowed index is [0, 14]", error.getMessage());
+    }
+
+    @Test
+    void getIndexThrowsWhenIndexTooLow() {
+        PUV puv = new PUV();
+
+        IndexOutOfBoundsException error = assertThrows(IndexOutOfBoundsException.class, () ->
+                puv.getIndex(-1));
+
+        assertEquals("Allowed index is [0, 14]", error.getMessage());
+    }
+
+    @Test
+    void getIndexThrowsWhenIndexTooHigh() {
+        PUV puv = new PUV();
+
+        IndexOutOfBoundsException error = assertThrows(IndexOutOfBoundsException.class, () ->
+                puv.getIndex(15));
+
+        assertEquals("Allowed index is [0, 14]", error.getMessage());
+    }
 }
