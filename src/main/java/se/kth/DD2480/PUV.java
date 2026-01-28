1bf9e637fb9563394d543de041b907a8a64dda24
Kevin Lov <Kevin.Looov@gmail.com>
2026-01-28 19:37:43 +0100

    feat: getter for specific index in PUV (#87)
    User can extract info from the array with this getter. Also uses the
    checkIndex method to validate what we want to check.

--- src/main/java/se/kth/DD2480/PUV.java
+++ src/main/java/se/kth/DD2480/PUV.java
@@ -3,13 +3,18 @@ package se.kth.DD2480;
 public class PUV {
     private boolean[] arr;
 
-    public PUV () {
+    public PUV() {
         arr = new boolean[15];
     }
 
     public void setIndex(int i, boolean value) {
         checkIndex(i);
         arr[i] = value;
+    }
+
+    public boolean getIndex(int i) {
+        checkIndex(i);
+        return arr[i];
     }
 
     private void checkIndex(int i) {
