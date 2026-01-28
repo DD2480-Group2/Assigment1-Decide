5517c97965c6565695ffbacafb0f83cc053ceab0
Kevin Lov <Kevin.Looov@gmail.com>
2026-01-28 19:46:46 +0100

    feat: Adding setUp for testclass (#87)
    Adding the setup for future methods to use.

--- src/test/java/se/kth/DD2480/PUVTest.java
+++ src/test/java/se/kth/DD2480/PUVTest.java
@@ -2,16 +2,21 @@ package se.kth.DD2480;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PUVTest {
+    private PUV puv;
 
     @BeforeEach
     void setUp() {
+        puv = new PUV();
     }
 
     @AfterEach
     void tearDown() {
     }
+
+
 }
