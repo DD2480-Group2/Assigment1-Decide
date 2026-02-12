8bc16ef3cfa79ff8cd82d1d23be6f79c9f6e3669
Teoman Köylüoglu <151879574+Laafayeette@users.noreply.github.com>
2026-02-12 04:42:51 +0100

    Update LCM.java

--- src/main/java/se/kth/DD2480/LCM.java
+++ src/main/java/se/kth/DD2480/LCM.java
@@ -9,7 +9,7 @@ public class LCM {
         this.arr = arr;
     }
 
-    //A random comment1 1223123132141234121212333
+    //A random comment1 1223123132141234121212333123
     public CONNECTORS[][] getArr() {
         return arr;
     }
